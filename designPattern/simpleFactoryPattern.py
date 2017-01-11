b609845ed6f1343fd2df3fe43839d0ab3e146986
tankeryang <youngzyang@outlook.com>
2017-01-11 22:56:30 +0800

    Fix: 修复简单工厂模式关于 @property bug 问题

--- designPattern/simpleFactoryPattern.py
+++ designPattern/simpleFactoryPattern.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-<<<<<<< HEAD
-print ('test')
-=======
 # 简单工厂模式：用一个单独的类来实例化对象。给我一个信号（参数），我将为你生产出相应的对象。
 # 需求：增加一个操作，不影响其他操作，比如开方。
 # 一个计算器操作类，+ - * / 继承它，还有一个工厂类。工厂根据需求（信号）返回相应的对象。
@@ -48,4 +45,3 @@ class OperationFactory(object):
     def create_operate(self, operate):
         oper = self.operation.get(operate)
         return oper
->>>>>>> dev_seal_de
