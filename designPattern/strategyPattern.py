09c1d8c940cf06a742346089154bae6f0aa7e99f
seal_de <seal.de@foxmail.com>
2017-01-12 17:53:34 +0800

    Mod: 修改策略模式测试

--- designPattern/strategyPattern.py
+++ designPattern/strategyPattern.py
@@ -8,7 +8,58 @@
 
 # 第一步：编写一个方法完成统计客户总价。
 # 第二步：增加打折算法。
-# 第三步：用简单工厂模式实现收费的新策略：正常收费，打折收费，满减收费。
-# 第四步：http://www.imooc.com/wiki/detail/id/137 学习策略模式结构，使用策略模式改善代码。
-# 第五步：将策略模式与工厂模式结合完成需求。
+# 第三步：用简单工厂模式实现收费的新策略：正常收费，打折收费，满减收费。（去除增加新算法时，改动其他算法的风险）
+# 第四步：http://www.imooc.com/wiki/detail/id/137 学习策略模式结构，将策略模式与工厂模式结合完成需求。
 '''
+
+
+class ChargeRule(object):
+    def charge(self, price, amount):
+        pass
+
+
+class NormalChargeRule(ChargeRule):
+    def charge(self, price, amount):
+        return price * amount
+
+
+class DiscountChargeRule(ChargeRule):
+    def __init__(self):
+        self.__rate = 1.0
+
+    def set_rate(self, rate):
+        self.__rate = rate
+
+    def charge(self, price, amount):
+        return price * amount * self.__rate
+
+
+class FullCutChargeRule(ChargeRule):
+    def __init__(self):
+        self.__full = 0.0
+        self.__cut = 0.0
+
+    def set_full_cut(self, full, cut):
+        self.__full = full
+        self.__cut = cut
+
+    def charge(self, price, amount):
+        total_price = price * amount
+        if total_price >= self.__full:
+            total_price -= self.__cut
+        return total_price
+
+
+class ChargeRuleFactory(object):
+    NORMAL_CHARGE = "normal"
+    DISCOUNT_CHARGE = "discount"
+    FULL_CUT_CHARGE = "full_cut"
+
+    charge_rules = {}
+    charge_rules["normal"] = NormalChargeRule()
+    charge_rules["discount"] = DiscountChargeRule()
+    charge_rules["full_cut"] = FullCutChargeRule()
+
+    def create_charge_rule(self, rule):
+        charge_rule = self.charge_rules[rule]
+        return charge_rule
--- designPattern/test/test_strategyPattern.py
+++ designPattern/test/test_strategyPattern.py
@@ -5,10 +5,40 @@ from designPattern.strategyPattern import *
 class MyTestCase(unittest.TestCase):
     '''
     def test_first_step(self):
-        cahshier_software = CashierSoftware()
-        cahshier_software.add_goods(5.0, 1)
-        cahshier_software.add_goods(9.9, 8)
-        self.assertEqual(84.2, cahshier_software.get_total_price())
+        cashier_software = CashierSoftware()
+        cashier_software.add_goods(price=5.0, amount=1)
+        cashier_software.add_goods(price=9.9, amount=8)
+        self.assertEqual(84.2, cashier_software.get_total_price())
+    '''
+
+    '''
+    def test_second_step(self):
+        cashier_software = CashierSoftware()
+        cashier_software.add_goods_normal_charge(price=5.0, amount=2)
+        cashier_software.add_goods_discount_charge(price=9.9, amount=5, rate=0.5)
+        self.assertEqual(34.75, cashier_software.get_total_price())
+    '''
+
+    '''
+    def test_third_step_normal(self):
+        factory = ChargeRuleFactory()
+        charge_rule = factory.create_charge_rule(factory.NORMAL_CHARGE)
+        total_price = charge_rule.charge(price=5.0, amount=2)
+        self.assertEqual(10, total_price)
+
+    def test_third_step_discount(self):
+        factory = ChargeRuleFactory()
+        charge_rule = factory.create_charge_rule(factory.DISCOUNT_CHARGE)
+        charge_rule.set_rate(0.8)
+        total_price = charge_rule.charge(price=5.0, amount=2)
+        self.assertEqual(8, total_price)
+
+    def test_third_step_full_cut(self):
+        factory = ChargeRuleFactory()
+        charge_rule = factory.create_charge_rule(factory.FULL_CUT_CHARGE)
+        charge_rule.set_full_cut(10, 3)
+        total_price = charge_rule.charge(price=5.0, amount=2)
+        self.assertEqual(7, total_price)
     '''
 
 if __name__ == '__main__':
